3b2a3933d45b6976d3a1708d2b0f34ffea8cbadd
Dominic Smith <dominic.smith@bristol.ac.uk>
2016-05-10 18:44:41 +0100

    Add search option

--- notebook/bkeep.py
+++ notebook/bkeep.py
@@ -1,14 +1,28 @@
 #!/usr/bin/env python
 
 import argparse
+import logging 
 import os
 import sys
 import time
 
-def PushBack(notes):
+logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
+log = logging.getLogger(__name__)
+
+def PushBack(notes, args):
     currentDir =  os.getcwd()
     fileName = currentDir+'/.bkeep'
     date = time.strftime("%x")
+
+    if args.findDate: 
+        with open(fileName, "r") as f:
+            contents = f.readlines()
+            f.close()
+            for i, line in enumerate(contents):
+                if notes in line:
+                    for l in contents[i:i+3]:  sys.exit("Found contents %s" %l)
+                else:
+                    sys.exit("Not found")
 
     try:
         with open(fileName, "a") as file:           
@@ -25,13 +39,20 @@ if __name__=='__main__':
     parser.add_argument('-n', '--notes', 
                         help='Push back note',
                         nargs="*")
+    parser.add_argument('-f', '--findDate', 
+                        help="Find dates referring to note pushed",
+                        action='store_true')
+                       
     args = parser.parse_args()
     
-    if len(args.notes) ==0:
+    if not len(sys.argv) > 1:
+        log.exception("Cannot find any input")
+        sys.exit(0)
+
+    if len(args.notes)==0:
         parser.print_help()
-        sys.exit(1)
                 
     for note in args.notes:
-        PushBack(note)
+        PushBack(note, args)
 
     sys.exit(0)
