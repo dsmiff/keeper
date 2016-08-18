3d5ab71abadafe5c0a94aaf1a42e072793ff41d8
Dominic Smith <dosmith@cern.ch>
2016-08-18 12:14:24 +0100

    Add bookutils class

--- utils/FindBook.py
+++ utils/FindBook.py
@@ -1,3 +1,4 @@
+# Dominic Smith <dosmith@cern.ch>
 #!/usr/bin/env python
 
 import argparse
@@ -5,7 +6,34 @@ import logging
 import os
 import sys
 
-def FindBook(name, path):
-    for root, dirs, files in os.walk(path):
-        if name in files:
-            return os.path.join(root, name)
+##__________________________________________________||
+class BookUtils(object):
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def FindBook(name, path):
+        for root, dirs, files in os.walk(path):
+            if name in files:
+                return os.path.join(root, name)
+
+    @staticmethod
+    def touch(path):
+        with open(path, 'a'):
+            os.utime(path, None)
+
+    @staticmethod
+    def readlines(name,path):
+        fileName = FindBook(name,path)
+        if fileName:
+            fd = open(fileName)
+            i = 0
+            spaces = 0
+            tabs = 0
+            for i, line in enumerate(fd):
+                spaces += line.count(' ')
+                tabs += line.count('\t')
+            fd.close()
+            return spaces, tabs, i+1
+        else:
+            print("File not found")
